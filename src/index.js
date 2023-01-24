2f8ae24d8dcfa6aa4af0eae7f948a9dfad567d05
Eduardo Scrobote <EduardoScroboteProfissional@gmail.com>
2023-01-24 01:55:38 -0300

    agora eu comentei o código para fixar melhor meu aprendizado, e permitir que eu progrida mais rapido. Nas proximas alterações eu irei adicionar o O aos Sqaures e uma forma do jogo diferenciar os mesmos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './style.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//! COMPONENTE 1
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    render() {  //**Aqui eu criei a função render dentro docomponente Square, e agora se você clicar em qualquer quadrado dentro do jogo da velha, seu estado null se tornara X
+        return (  
+            <button className="square"
+            onClick={() => this.setState({value: "alterei" })} //** aqui está a função que transforma null em X, na linha 9 a função this.state={ value: null,} ela determina o state       
+            >; //** Como nulo ou sejá os quadrados ficaram vazios enquanto o setState muda para X
+                {this.state.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    //! COMPONENTE 1
+
+    //! COMPONENTE 2
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+} // ! COMPONENTE 2
+
+//! COMPONENTE 3
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+//! COMPONENTE 3
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
